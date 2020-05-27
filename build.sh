5c4573b8639f2910944476afe851c7c49523eb67
Jonathan Goble <jcgoble3@didactex.com>
2020-05-27 10:03:42 -0400

    Add missing cfitsio variables to build script

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ export API=28
 #for TARGET in aarch64-linux-android armv7a-linux-androideabi i686-linux-android x86_64-linux-android
 #do
     export TARGET=$1
+
     case $TARGET in
         aarch64*)
             export ABI=arm64-v8a
@@ -21,19 +22,25 @@ export API=28
             export ABI=x86-64
             ;;
     esac
+
     export CC=$TOOLCHAIN/bin/$TARGET$API-clang
     export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+
     if [[ "$TARGET" = "armv7a-linux-androideabi" ]]
     then
         export TRIPLE=arm-linux-androideabi
     else
         export TRIPLE=$TARGET
     fi
+
     export AR=$TOOLCHAIN/bin/$TRIPLE-ar
     export AS=$TOOLCHAIN/bin/$TRIPLE-as
     export LD=$TOOLCHAIN/bin/$TRIPLE-ld
     export RANLIB=$TOOLCHAIN/bin/$TRIPLE-ranlib
     export STRIP=$TOOLCHAIN/bin/$TRIPLE-strip
+
+    export CFITS_INC="-I/home/jonathan/work/build/arm64-v8a/include/"
+    export CFITS_SLIB="/home/jonathan/work/build/arm64-v8a/lib/libcfitsio.a"
 
     make
 #done
